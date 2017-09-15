ae872162024ce4813c706c52434d62e09a33cd0d
josephjripple <josephjripple@gmail.com>
2017-09-15 12:43:18 -0400

    retry of unsuccessful code for to-do list

--- app.js
+++ app.js
@@ -6,45 +6,30 @@ function onReady() {
   addToDoForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    //get the text
+    //convert text value input into to-do to "title"
     let title = newToDoText.value;
 
-    //create a new line
+    //create a new li
     let newLi = document.createElement('li');
 
-    //create a new input for the checkbox
+    //create a new input for a checkbox
     let checkbox = document.createElement('input');
 
-    //set the input's type to checkbox
+    //set the input type to checkbox
     checkbox.type = "checkbox";
 
-    //set the title
+    //set new li text content equal to title variable (the user input text)
     newLi.textContent = title;
 
-    //attach new li to ul
+    //attach new li to to do list
     toDoList.appendChild(newLi);
 
-    //attach the checkbox to the li
+    //attach(append) checkbox to the li
     newLi.appendChild(checkbox);
 
-    //empty the input
-    newToDoText.value = '';
+    //reset input value to empty
+    newToDoText.value = "";
 
-    //create a new input for the delete box
-    let deleteButton = document.createElement('input');
-
-    //set the delete button type to button
-    deleteButton.type = "button";
-
-    //attach delete button to the li
-    newLi.appendChild(deleteButton);
-
-    deleteButton.addEventListener('submit', (event) => {
-      var listItems = document.getElementsByTagName("li");
-        for (var i = 0; i < listItems.length; i++) {
-          listItems[i].onclick = function() {this.parentNode.removeChild(this);};
-}
-    });
 
   });
 }
