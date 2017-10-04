2c1a787c71f7aa90cd49e627688d1f6fc8e34e9a
josephjripple <josephjripple@gmail.com>
2017-10-04 13:41:39 -0400

    add delete button functionality

--- app.js
+++ app.js
@@ -15,8 +15,14 @@ function onReady() {
     //create a new input for a checkbox
     let checkbox = document.createElement('input');
 
+    //create a delete button for li items
+    let deleteButton = document.createElement('input');
+
     //set the input type to checkbox
     checkbox.type = "checkbox";
+
+    //set the delete button type to button
+    deleteButton.type = "radio";
 
     //set new li text content equal to title variable (the user input text)
     newLi.textContent = title;
@@ -26,6 +32,19 @@ function onReady() {
 
     //attach(append) checkbox to the li
     newLi.appendChild(checkbox);
+
+    //attach(append) delete button to li
+    newLi.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", function() {
+          var todo = document.querySelectorAll("LI");
+          var input = document.querySelectorAll("INPUT");
+              for(i = 0; i < input.length; i++){
+                  if(input[i].checked === true){
+                          input[i].parentNode.remove();
+                  }
+              }
+      });
 
     //reset input value to empty
     newToDoText.value = "";
